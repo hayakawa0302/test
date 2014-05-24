07f0a8119a143d7e7ae7bbe9aa4029665273b61d
super hayakawa <melty_heaven@hotmail.com>
2014-05-24 16:47:45 +0900

    Update 2.c

--- 2.c
+++ 2.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-	int i;
-	i = 0;
-	while (i < 10){
-		i++;
-		printf("Hello World\n");
-	}
-	return 0;
+    int i;
+    i = 0;
+    while (i < 10){
+        i++;
+        printf("Hello World\n");
+    }
+    return 0;
 }
